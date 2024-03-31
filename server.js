6b7470e84a47cff58a2c8376c056346519c05e1b
Ayush Singh Aryan <ayush.singh2019@vitbhopal.ac.in>
2024-03-31 10:40:20 +0530

    changes server.js location

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-const Note = require('./models/Note');
+const Note = require('./src/models/Note');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //! if extended true means we can give nested objects
@@ -18,7 +18,7 @@ mongoose.connect('mongodb+srv://sharksingh700:pdOni4CT7uqvEM5J@cluster0.ngg9sgo.
          const response = {message:"API works"};
          res.json(response);
       });
-      const noteRouter = require('./routes/Note');
+      const noteRouter = require('./src/routes/Note');
       app.use('/notes',noteRouter); //! asey use karney sey my url like /notes/notes/add , so i want /notes only one time so remove /notes from Note router
 
 
